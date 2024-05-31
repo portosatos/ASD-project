d6e7e97f0be8c5adb818967b345e425786b179be
aibls <builasheva_a@auca.kg>
2024-05-31 11:05:23 +0600

    Class Task is added with description and completion status

--- project.cs
+++ project.cs
@@ -1,1 +1,20 @@
-aaa
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Task
+{
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public Task(string description)
+    {
+        Description = description;
+        IsCompleted = false;
+    }
+    public override string ToString()
+    {
+        return $"{Description} (Completed: {IsCompleted})";
+    }
+}
+
